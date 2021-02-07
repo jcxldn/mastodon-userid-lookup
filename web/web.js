edcefecf4be5bb3a710636780c9492afb6657afc
James <jcahill773@gmail.com>
2021-02-07 21:14:57 +0000

    Change web js to use newer jquery + es syntax

--- web/web.js
+++ web/web.js
@@ -5,35 +5,31 @@ import jQuery from "jquery"
 
 const mastodonFullRegex = /^(@)[a-z0-9_]+([a-z0-9_\.]+[a-z0-9_]+)?(@)([a-z0-9_]+([a-z0-9_\.]+[a-z0-9_]+))(.)([a-z0-9_]+([a-z0-9_\.]+[a-z0-9_]+))/i;
 
-jQuery("form").ready(function () {
-  jQuery("form").submit(function (e) {
-    console.log("SUBMIT");
+jQuery(() => {
+  jQuery("form").on("submit", e => {
+    console.log("Submit event (enter button pressed)");
     e.preventDefault();
     validate();
-  });
+  })
 
-  jQuery("button").click(function (e) {
-    console.log("BTN CLICK");
+  jQuery("button").on("click", e => {
+    console.log("Click event (Lookup button clicked)");
     e.preventDefault();
     validate();
-  });
-});
+  })
+})
 
-function validate() {
+const validate = () => {
   jQuery(".res").html("Searching...");
-  console.clear();
+
   const username = jQuery("#username").val();
 
   const isValid = mastodonFullRegex.test(username);
 
-  console.log(`is valid: jQuery{isValid}`);
-
   if (isValid) {
     lookup(username).then(uid => {
-        console.log(`uid: ${uid}`);
         jQuery(".res").html(`User found!<br> Your ID is <b>${uid}</b>.</br>`);
     }).catch(err => {
-        console.log("in err:" + err)
         console.error(err)
         jQuery(".res").html("User or domain not found. Please try again.");
     })
